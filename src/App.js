69deb7ce9e410b5435e092af23ceaa12cea5d0d7
Christopher Tero <christopher.tero@gmail.com>
2019-09-24 16:39:37 -0500

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
+import ListValues from './Components/ListValues'
 import './App.css';
 
 export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      projects: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://localhost:5000/api/values")
+      .then(response => response.json())
+      .then(result => this.setState({projects: result}))
+  }
 
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        {this.state.projects ? <ListValues projects={this.state.projects} /> : ""}
       </div>
     );
   }
---
+++ src/Components/ListValues.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ListValues(props) {
+  console.log(props.projects)
+  // const listProjects = props.projects.map((project) => {
+  //   return(
+  //     <p>{project.title}</p>
+  //   )
+  // })
+
+  return(
+    <div>
+    <p>Some value</p>
+    </div>
+  )
+}
+// {listProjects}
